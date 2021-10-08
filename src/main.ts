73981ccad7ce8297d719fbc4f3d87482b0dc630d
eriawan <eriawan@gti.co.id>
2021-10-08 21:48:44 +0700

    heroku configuration

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
   // app.use(passport.initialize());
   // app.use(passport.session());
 
-  await app.listen(3000);
+  await app.listen(3000 || process.env.PORT);
 }
 bootstrap();
